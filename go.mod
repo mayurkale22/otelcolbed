0da24882a72c448524bdec49b413f8c17674771e
Mayur Kale <mayurkale@google.com>
2020-01-06 17:33:13 -0800

    first commit

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/open-telemetry/opentelemetry-collector/otelcolbed
+
+go 1.12
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/census-instrumentation/opencensus-proto v0.2.1
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/golang/protobuf v1.3.2
+	github.com/open-telemetry/opentelemetry-collector v0.2.1
+	github.com/open-telemetry/opentelemetry-collector-contrib/exporter/stackdriverexporter v0.0.0-20200106205500-3faee112ab81
+	github.com/shirou/gopsutil v2.18.12+incompatible
+	github.com/spf13/viper v1.4.1-0.20190911140308-99520c81d86e
+	github.com/stretchr/testify v1.4.0
+	go.uber.org/zap v1.10.0
+)
+
+replace github.com/open-telemetry/opentelemetry-collector => ../
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"log"
+	
+	"github.com/open-telemetry/opentelemetry-collector/config"
+  "github.com/open-telemetry/opentelemetry-collector/exporter"
+  "github.com/open-telemetry/opentelemetry-collector/extension"
+  "github.com/open-telemetry/opentelemetry-collector/internal/version"
+  "github.com/open-telemetry/opentelemetry-collector/processor"
+  "github.com/open-telemetry/opentelemetry-collector/receiver"
+  "github.com/open-telemetry/opentelemetry-collector/service"
+ 	"github.com/open-telemetry/opentelemetry-collector/receiver/opencensusreceiver"
+ 	"github.com/open-telemetry/opentelemetry-collector/extension/pprofextension"
+  "github.com/open-telemetry/opentelemetry-collector/processor/queuedprocessor"
+  "github.com/open-telemetry/opentelemetry-collector/processor/batchprocessor"
+  "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/stackdriverexporter"
+)
+
+func main() {
+	handleErr := func(err error) {
+		if err != nil {
+			log.Fatalf("Failed to run the service: %v", err)
+		}
+	}
+
+	extensions, err := extension.Build(
+		&pprofextension.Factory{},
+	)
+	handleErr(err)
+
+	receivers, err := receiver.Build(
+		&opencensusreceiver.Factory{},
+	)
+	handleErr(err)
+
+	exporters, err := exporter.Build(
+		&stackdriverexporter.Factory{},
+	)
+	handleErr(err)
+
+	processors, err := processor.Build(
+		&queuedprocessor.Factory{},
+		&batchprocessor.Factory{},
+	)
+	handleErr(err)
+
+	factories := config.Factories{
+		Extensions: extensions,
+		Receivers:  receivers,
+		Processors: processors,
+		Exporters:  exporters,
+	}
+
+	info := service.ApplicationStartInfo{
+		ExeName:  "otelcol",
+		LongName: "OpenTelemetry Collector",
+		Version:  version.Version,
+		GitHash:  version.GitHash,
+	}
+
+	svc, err := service.New(factories, info)
+	handleErr(err)
+
+	err = svc.Start()
+	handleErr(err)
+}
+
+// package main
+
+// import (
+// 	"log"
+
+// 	"github.com/open-telemetry/opentelemetry-collector/defaults"
+// 	"github.com/open-telemetry/opentelemetry-collector/internal/version"
+// 	"github.com/open-telemetry/opentelemetry-collector/service"
+// )
+
+// func main() {
+// 	handleErr := func(err error) {
+// 		if err != nil {
+// 			log.Fatalf("Failed to run the service: %v", err)
+// 		}
+// 	}
+
+// 	factories, err := defaults.Components()
+// 	handleErr(err)
+
+// 	info := service.ApplicationStartInfo{
+// 		ExeName:  "otelcol",
+// 		LongName: "OpenTelemetry Collector",
+// 		Version:  version.Version,
+// 		GitHash:  version.GitHash,
+// 	}
+
+// 	svc, err := service.New(factories, info)
+// 	handleErr(err)
+
+// 	err = svc.Start()
+// 	handleErr(err)
+// }
